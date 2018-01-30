38a1808cb801848a7fa6c80296a0cefb5c25c237
Melinda Yeoh <myeoh27@gmail.com>
2018-01-30 17:56:37 -0500

    updated scramble.js

--- src/scramble.js
+++ src/scramble.js
@@ -1,24 +1,23 @@
+//todo: refactor this code
 function scrambleString(stringPhrase) {
   const numCharactersInString = stringPhrase.length;
   const indexesUsedAlready = [];
   const scrambledString = [];
 
-  for(let i = 0; i < numCharactersInString; i++) {
+  for (let i = 0; i < numCharactersInString; i++) {
     const randomNum = Math.floor(Math.random() * numCharactersInString);
 
     if (!indexesUsedAlready.includes(randomNum)) {
       indexesUsedAlready.push(randomNum);
 
-      scrambledString.push(stringPhrase[randomNum])
+      scrambledString.push(stringPhrase[randomNum]);
     }
   }
 
   if (indexesUsedAlready.length !== numCharactersInString) {
-    // console.log('Got here!');
-    // console.log(`indexesUsedAlready: ${indexesUsedAlready}`);
 
     // Find the indexes that weren't used and push it to scrambledString
-    for(let i = 0; i < numCharactersInString; i++) {
+    for (let i = 0; i < numCharactersInString; i++) {
       if (!indexesUsedAlready.includes(i)) {
         scrambledString.push(stringPhrase[i]);
       }
